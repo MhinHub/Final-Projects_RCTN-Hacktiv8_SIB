7d1bc00410213001e4b75998de095c07c2e715a7
MhinHub <iskandarm20d@student.unhas.ac.id>
2022-10-15 00:37:58 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# Halo, Selamat Datang di Repositori Kami 🥳
+### Ini adalah repositori semua file final projek yang kami buat dalam program Studi Independen Hacktiv8 kelas React.
--- news-site/src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import Reducer from "../features/news-slice"
-
-export const store = configureStore({
-    reducer: {
-        news: Reducer,
-    },
-});
