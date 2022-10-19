d53a9e9807f4a87c2cc0c09efed595f6e8b360bd
MhinHub <iskandarm20d@student.unhas.ac.id>
2022-10-19 14:12:12 +0800

    Fix error

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -16,7 +16,7 @@ export default ({ category }) => {
   return (
     <>
       <div className="grid lg:grid-cols-2 sm:grid-cols-1 gap-x-12 gap-y-10 mx-44 my-10 justify-center px-20">
-        {news.articles?.slice(0, 4).map((news, index) => (
+        {news?.articles?.slice(0, 4).map((news, index) => (
           <CardItem key={index} news={news} />
         ))}
       </div>
