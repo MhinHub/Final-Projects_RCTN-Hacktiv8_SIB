fe045917dcc4114b217e705f0981d4c22a2331d6
MhinHub <iskandarm20d@student.unhas.ac.id>
2022-10-19 16:57:33 +0800

    update some code

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,30 +1,12 @@
 export default () => {
     return (
-        <aside className="float-left p-4 m-2 w-[15%] border border-gray-900 rounded-md fixed">
-            <div className="flex flex-col">
-                <h6 className="text-lg font-bold"><b>Categories</b></h6>
-                <ul className="pl-4 text-base font-light">
-                    <li className="list-disc">Indonesia</li>
-                    <li>Programming</li>
-                    <li>Covid-19</li>
-                    <li>Sports</li>
-                    <li>Technology</li>
-                    <li>Entertainment</li>
-                </ul>
-                <hr />
-                <h6 className="text-lg font-bold"><b>Manage</b></h6>
-                <ul className="pl-4 text-base font-light">
-                    <li>Saved</li>
-                    <li>Readed</li>
-                </ul>
-                <hr />
-                <h6 className="text-lg font-bold"><b>Theme</b></h6>
-                <ul className="pl-4 list-inside">
-                    <li className="flex flex-row gap-2">
-                        <input type="radio" name="radio-1" className="radio"/>
-                        <input type="radio" name="radio-1" className="radio radio-primary"/>
-                        <input type="radio" name="radio-1" className="radio radio-secondary"/>
-                    </li>
+        <aside className="float-left side-bar">
+            <div className="absolute">
+                <h4 className="px-14 py-px bg-primary text-base-100 text-center mb-7"><strong>Popular</strong></h4>
+                <ul className="list-disc list-outside">
+                    <li className="text-base"><h6 className="text-gray-400">27 Maret 2021</h6></li>
+                    <p className="mt-1 text-sm text-primary leading-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero und</p>
+                    <hr className="mb-3" />
                 </ul>
             </div>
         </aside >
