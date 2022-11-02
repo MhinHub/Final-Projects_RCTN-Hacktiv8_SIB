f4f2f1b1ee36ebeafb0367cdc4dedad7abbe5f23
Irfan Zidni <irfanzidniofficial@gmail.com>
2022-11-03 06:39:57 +0700

    image carousel home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,17 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <div className="bg-blue-600 py-1">
+    <div className="bg-blue-600 shadow-md">
       <div className="navbar bg-base-100">
         <div className="flex-1">
           <a className="btn-ghost btn text-xl normal-case">daisyUI</a>
+        </div>
+        <div className="form-control pr-5">
+          <input
+            type="text"
+            placeholder="Search"
+            className="input-bordered input"
+          />
         </div>
         <div className="flex-none">
           <div className="dropdown-end dropdown">
@@ -43,30 +50,9 @@ export default function Navbar() {
               </div>
             </div>
           </div>
-          <div className="dropdown-end dropdown">
-            <label tabIndex={0} className="btn-ghost btn-circle avatar btn">
-              <div className="w-10 rounded-full">
-                <img src="https://placeimg.com/80/80/people" />
-              </div>
-            </label>
-            <ul
-              tabIndex={0}
-              className="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-100 p-2 shadow"
-            >
-              <li>
-                <a className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </a>
-              </li>
-              <li>
-                <a>Settings</a>
-              </li>
-              <li>
-                <a>Logout</a>
-              </li>
-            </ul>
-          </div>
+        </div>
+        <div className="px-5">
+          <a className="btn">Masuk</a>
         </div>
       </div>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,54 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div className="w-full max-w-lg">
-      <h4>Hello React</h4>
-      <p className="text-lg leading-relaxed text-gray-400">
-        A JavaScript library for building user interfaces
-      </p>
+    <div className="w-full pt-5">
+      <div className="carousel w-full">
+        <div id="slide1" className="carousel-item relative w-full ">
+          <img src="https://placeimg.com/1240/400/arch" className="w-full" />
+          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+            <a href="#slide4" className="btn-circle btn">
+              ❮
+            </a>
+            <a href="#slide2" className="btn-circle btn">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide2" className="carousel-item relative w-full">
+          <img src="https://placeimg.com/1240/400/laptop" className="w-full" />
+          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+            <a href="#slide1" className="btn-circle btn">
+              ❮
+            </a>
+            <a href="#slide3" className="btn-circle btn">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide3" className="carousel-item relative w-full">
+          <img src="https://placeimg.com/1240/400/digital" className="w-full" />
+          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+            <a href="#slide2" className="btn-circle btn">
+              ❮
+            </a>
+            <a href="#slide4" className="btn-circle btn">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide4" className="carousel-item relative w-full">
+          <img src="https://placeimg.com/800/200/arch" className="w-full" />
+          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+            <a href="#slide3" className="btn-circle btn">
+              ❮
+            </a>
+            <a href="#slide1" className="btn-circle btn">
+              ❯
+            </a>
+          </div>
+        </div>
+      </div>
+      <h1 className="text-lg">Produk Terlaris</h1>
     </div>
   );
 }
